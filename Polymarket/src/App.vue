3a61adcd314ccf50beab93b38085a15d40c4a08d
Boycool3 <christian.beaz91@gmail.com>
2026-04-29 12:00:40 -0400

    HHH

--- Polymarket/src/App.vue
+++ Polymarket/src/App.vue
@@ -5,7 +5,22 @@
 </template>
 
 <script setup>
+import { ref, onMounted } from 'vue'
+import { supabase } from './supabase'
 
+const transactions = ref([])
+const error = ref(null)
+
+onMounted(async () => {
+  let { data: transactionData, error: err } = await supabase
+    .from('transactions')
+    .select('*')
+  if (err) {
+    error.value = err.message
+  } else {
+    transactions.value = transactionData
+  }
+})
 </script>
 
 <style scoped>
