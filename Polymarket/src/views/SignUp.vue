cacd8f70cd771f9ef7cd2c392dc0b598f9c047e7
Your Name <ivang112@nycstudents.net>
2026-04-29 12:30:47 -0400

    updates

---
+++ Polymarket/src/views/SignUp.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="SignUp">
+        <h2>Sign Up</h2>
+        <input type="text" placeholder="Username" />
+        <input type="text" placeholder="Email" />
+        <input type="password" placeholder="Password" />
+        <input type="text" placeholder="Confirm Password" />
+        <button>Sign Up</button>
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
