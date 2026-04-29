95291d47537162f0e2f908ff2bedf848940f8c9f
Boycool3 <christian.beaz91@gmail.com>
2026-04-29 12:16:45 -0400

    sytliishing

--- Polymarket/src/views/Home.vue
+++ Polymarket/src/views/Home.vue
@@ -9,5 +9,25 @@
 </script>
 
 <style scoped>
+:root {
+  --bg-lm: #1e2b3b;
+  --text-lm: #f0f3f5;
+  --outline-lm: #34485d;
+  --divs-lm: #2f3f4f;
+  --lightish-lm: #8d9aad;
+  --yestext-lm: #3fd371;
+  --yes-lm: #345250;
+  --notext-lm: #d5363b;
+  --no-lm: #5a414c;
 
+  --primary: var(--bg-lm);
+  --secondary: var(--text-lm);
+  --tertiary: var(--outline-lm);
+  --quaternary: var(--divs-lm);
+  --quinary: var(--lightish-lm);
+  --senary: var(--yestext-lm);
+  --septenary: var(--yes-lm);
+  --octonary: var(--notext-lm);
+  --nonary: var(--no-lm);
+}
 </style>
