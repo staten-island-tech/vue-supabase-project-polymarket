3d68110736666c9b898f565e56d0b618540810fb
Your Name <ivang112@nycstudents.net>
2026-04-30 12:08:12 -0400

    s

--- Polymarket/src/router/index.js
+++ Polymarket/src/router/index.js
@@ -1,8 +1,15 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import SignUp from '../views/SignUp.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [],
+  routes: [
+    {
+      path: '/sign-up',
+      name: 'SignUp',
+      component: SignUp
+    }
+  ]
 })
 
 export default router
