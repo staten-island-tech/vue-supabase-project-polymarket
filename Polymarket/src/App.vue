62271f2aa3653f0507a8bbb55e7ba7f802ce0792
Vinsonc6 <vinsonc6@nycstudents.net>
2026-05-04 12:32:50 -0400

    e
    e

--- Polymarket/src/App.vue
+++ Polymarket/src/App.vue
@@ -1,11 +1,41 @@
-<script setup></script>
+<script setup>
+import { onMounted, ref } from "vue";
+import { useRouter } from "vue-router";
+import { supabase } from "@/supabaseClient";
+
+const router = useRouter();
+const session = ref(null);
+
+async function loadSession() {
+  const { data } = await supabase.auth.getSession();
+  session.value = data.session;
+
+  if (!data.session) {
+
+    if (router.currentRoute.value.path !== "/login") {
+      router.push("/login");
+    }
+  }
+}
+
+async function handleAuthChanges() {
+  supabase.auth.onAuthStateChange((_event, newSession) => {
+    session.value = newSession;
+
+    if (!newSession) {
+      if (router.currentRoute.value.path !== "/login") {
+        router.push("/login");
+      }
+    }
+  });
+}
+
+onMounted(async () => {
+  await loadSession();
+  await handleAuthChanges();
+});
+</script>
 
 <template>
-  <h1>You did it!</h1>
-  <p>
-    Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
-    documentation
-  </p>
+  <router-view />
 </template>
-
-<style scoped></style>
