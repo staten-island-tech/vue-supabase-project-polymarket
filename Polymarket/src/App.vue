1a3363d9cf693689c5034166224a34aebbb6edee
Vinsonc6 <vinsonc6@nycstudents.net>
2026-05-04 12:30:47 -0400

    e
    e

--- Polymarket/src/App.vue
+++ Polymarket/src/App.vue
@@ -1,13 +1,12 @@
+<script setup></script>
+
 <template>
-  <div>
-    <RouterLink to="/sign-up">Go to Sign Up</RouterLink>
-  </div>
+  <h1>You did it!</h1>
+  <p>
+    Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
+    documentation
+  </p>
 </template>
 
-<script setup>
+<style scoped></style>
 
-</script>
-
-<style>
-
-</style>
